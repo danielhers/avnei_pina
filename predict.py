ae5a8b08d155db1b7486ee109d081be39e2fd22a
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-03-13 14:27:43 +0200

    Predict all possible continuations

--- predict.py
+++ predict.py
@@ -2,18 +2,20 @@
 
 import sys
 import csv
+from itertools import product
+
 
 def ngram(L, n, C):
-    T = tuple(L[-n+1:])
-    try:
-        return max(C[T])[1]
-    except:
-        print("Not found: " + str(T))
+    T = L[-n+1:]
+    return sorted(set(t for l in product(*T) if l in C for t in max(C[l].items())[1]))
 
-def predict(L, n, counts_file, m):
-    while len(L) < m:
-        L.append(ngram(L, n, C))
-    return L
+
+def predict(L, n, C, m):
+    P = [[l] for l in L]
+    while len(P) < m:
+        P.append(ngram(P, n, C))
+    return P
+
 
 if len(sys.argv) < 4:
     print("Usage: %s counts m word ..." % sys.argv[0])
@@ -22,5 +24,6 @@ with open(sys.argv[1]) as f:
     C = {}
     for l in csv.reader(f, delimiter="\t"):
         t = l[0].split() 
-        C.setdefault(tuple(t[:-1]), []).append((int(l[1]), t[-1]))
-print(" ".join(predict(list(sys.argv[3:]), 1+len(next(iter(C.keys()))), C, int(sys.argv[2]))))
+        C.setdefault(tuple(t[:-1]), {}).setdefault(int(l[1]), []).append(t[-1])
+P = predict(sys.argv[3:], 1+len(next(iter(C.keys()))), C, int(sys.argv[2]))
+print(" ".join(map("/".join, P)))
