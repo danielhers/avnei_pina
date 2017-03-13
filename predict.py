48bf9b99f0f5d87d75863eccb309ef0d43b2bf36
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2017-03-13 17:35:31 +0200

    POS tagging algorithm

--- predict.py
+++ predict.py
@@ -5,9 +5,12 @@ import csv
 from itertools import product
 
 
-def ngram(L, n, C):
-    T = L[-n+1:]
+def select(T, C):
     return sorted(set(t for l in product(*T) if l in C for t in max(C[l].items())[1]))
+
+
+def ngram(L, n, C):
+    return select(L[-n+1:], C)
 
 
 def predict(L, n, C, m):
@@ -17,13 +20,19 @@ def predict(L, n, C, m):
     return P
 
 
-if len(sys.argv) < 4:
-    print("Usage: %s counts m word ..." % sys.argv[0])
-    sys.exit(1)
-with open(sys.argv[1]) as f:
-    C = {}
-    for l in csv.reader(f, delimiter="\t"):
-        t = l[0].split() 
-        C.setdefault(tuple(t[:-1]), {}).setdefault(int(l[1]), []).append(t[-1])
-P = predict(sys.argv[3:], 1+len(next(iter(C.keys()))), C, int(sys.argv[2]))
-print(" ".join(map("/".join, P)))
+def load_counts(filename):
+    with open(filename) as f:
+        C = {}
+        for l in csv.reader(f, delimiter="\t"):
+            t = l[0].split() 
+            C.setdefault(tuple(t[:-1]), {}).setdefault(int(l[1]), []).append(t[-1])
+    return C
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: %s counts m word ..." % sys.argv[0])
+        sys.exit(1)
+    C = load_counts(sys.argv[1])
+    P = predict(sys.argv[3:], 1+len(next(iter(C.keys()))), C, int(sys.argv[2]))
+    print(" ".join(map("/".join, P)))
---
+++ tag.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import sys
+from itertools import product
+from functools import reduce
+from operator import mul
+
+from predict import load_counts, select
+
+
+def common(xs):
+    return set(xs[0]).intersection(*xs[1:])
+
+
+def prod(items):
+    dicts = [{tuple(v): k for k, v in d} for d in items]
+    return [(reduce(mul, (d[i] for d in dicts)), i) for i in common(dicts)]
+    
+
+def select2(T, C):
+    return max(prod([c[(t,)].items() for l in product(*T) for t, c in zip(l, C)]))[1]
+
+
+def tag2(L, Cwt, Ct2):
+    T = [select([L[0]], Cwt)]
+    for l in L[1:]:
+        T.append(select2(([l], T[-1]), (Cwt, Ct2)))
+    return T
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: %s word_tag tag_ngram word ..." % sys.argv[0])
+        sys.exit(1)
+    Cwt, Ct2 = map(load_counts, sys.argv[1:3])
+    T = tag2(sys.argv[3:], Cwt, Ct2)
+    print(" ".join(map("/".join, T)))
