d5ae6e72d206629ab8b574d02fccda157c9d443d
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-05-30 12:01:47 +0300

    Add spacer and stick correct+incorrect

--- 2018/Exercise 3/viz.sh
+++ 2018/Exercise 3/viz.sh
@@ -9,12 +9,12 @@ for d in correct incorrect; do
     udapy write.Tikz < $f > $tex
     sed -i 's/\$/\\$/g' $tex || exit 1
     sed -i '2i\\\\pagestyle{empty}' $tex || exit 1
-    ~/bin/textogif -png $tex || exit 1
+    ~/bin/textogif -dpi 300 -png $tex || exit 1
     rm -f $tex
   done
   cd ..
 done
 for f in correct_tex/*.png; do
-  convert $f incorrect_tex/`basename $f` +append `basename $f` || exit 1
+  convert $f spacer.png incorrect_tex/`basename $f` +append -transparent white -trim `basename $f` || exit 1
 done
 rm -rf correct_tex incorrect_tex
